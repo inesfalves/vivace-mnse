2ee6004cbd5b64748340aee6b9dddd07156921ca
Nokas <up201605335@fe.up.pt>
2021-04-15 22:58:24 +0100

    restore deleted files

--- Project/src/index.js
+++ Project/src/index.js
@@ -1,9 +1,12 @@
-import './index.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css'
 
 ReactDOM.render(
-  <h1>VIVACE</h1>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
