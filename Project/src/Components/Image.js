c14c94b087238268e4d6e619ce7a753011d722bf
Nokas <up201605335@fe.up.pt>
2021-04-22 17:56:07 +0100

    add artists' images to the main page

---
+++ Project/src/Components/Image.js
@@ -0,0 +1,9 @@
+import gaga from "../Lady-Gaga.png";
+
+function Image(props) {
+  return (
+    <img src={props.title} alt="Artist Picture" width="600" height="750" />
+  );
+}
+
+export default Image;
--- Project/src/Components/Pages/HomePage.js
+++ Project/src/Components/Pages/HomePage.js
@@ -1,20 +1,29 @@
 import "../../App.css";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 import Button from "../Button.js";
+import Image from "../Image.js";
+import gaga from "../../Lady-Gaga.png";
+import elvis from "../../Elvis_Presley.png";
 
 function HomePage() {
-    return (
-        <div className="HomePage">
-            <header className="Home-header">
-                <h1>VIVACE</h1>
-                <p className="Home-desc">An interactive timeline for pop music lovers</p>
-            </header>
-            <Link to='/example'>
-                <Button/>
-            </Link>
-            <p className="Home-desc">Press Play to continue</p>
-        </div>
-    );
+  return (
+    <div className="PageLayout">
+      <Image title={elvis}></Image>
+      <div className="HomePage">
+        <header className="Home-header">
+          <h1>VIVACE</h1>
+          <p className="Home-desc">
+            An interactive timeline for pop music lovers
+          </p>
+        </header>
+        <Link to="/example">
+          <Button />
+        </Link>
+        <p className="Home-desc">Press Play to continue</p>
+      </div>
+      <Image title={gaga}></Image>
+    </div>
+  );
 }
 
 export default HomePage;
